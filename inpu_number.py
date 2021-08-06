65b7f8ed23b3a63aa5376876d5ded56c55d14908
satu123sa <88521000+satu123sa@users.noreply.github.com>
2021-08-06 15:00:50 +0700

    Add files via upload
    menambahkan inpu_number.py

---
+++ inpu_number.py
@@ -0,0 +1,16 @@
+#Belajar Input Number
+
+print("================== Selamat Datang di Kalkulator Sederhana =================")
+
+
+print("Masukkan Angka Pertama : ")
+a = int (input ())
+
+
+print("Masukkan Angka Kedua : ")
+b = int (input ())
+
+
+hasil = a * b
+
+print(f"{a} * {b} = {hasil}")
